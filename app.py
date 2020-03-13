ab9bdf9b664cb3eac04fa6efa7098faa40059d24
jackokramer <jackokramer@gmail.com>
2020-03-13 09:26:41 -0700

    third

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, session, redirect, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/contact', methods=['POST'])
+def contact():
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
